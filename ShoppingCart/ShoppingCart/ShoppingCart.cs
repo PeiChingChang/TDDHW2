417937ca3a637bb0dbff8316a1a27b8b5d4e2497
peiching <leslie7892001@hotmail.com>
2016-01-20 11:15:00 +0800

    第一集買了一本_共買一本_價格為100*1(綠燈)

--- ShoppingCart/ShoppingCart/ShoppingCart.cs
+++ ShoppingCart/ShoppingCart/ShoppingCart.cs
@@ -10,6 +10,20 @@ namespace ShoppingCart
         public int CalculateAmount(List<book> bookCollection)
         {
             int amount = 0;
+
+            if (bookCollection == null || bookCollection.Count() == 0)
+                return amount;
+
+            var diffBook = bookCollection.Distinct(o => o.Id);
+            var sameBook = bookCollection.Except(diffBook);
+
+            switch (diffBook.Count())
+            {
+                case 1:
+                    amount = Convert.ToInt32(Math.Ceiling(diffBook.Sum(o => o.Price) * 1.0 + sameBook.Sum(o => o.Price)));
+                    break;
+            }
+
             return amount;
         }
     }
@@ -21,5 +35,21 @@ namespace ShoppingCart
         public string Name { get; set; }
 
         public int Price { get; set; }
+    }
+
+    public static class EnumerableExtender
+    {
+        public static IEnumerable<TSource> Distinct<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                var elementValue = keySelector(element);
+                if (seenKeys.Add(elementValue))
+                {
+                    yield return element;
+                }
+            }
+        }
     }
 }
