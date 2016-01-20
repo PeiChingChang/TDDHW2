14619d259fa42fb4e019075ca2248e564acf2158
peiching <leslie7892001@hotmail.com>
2016-01-20 10:46:50 +0800

    第一集買了一本_共買一本_價格為100*1(紅燈)

---
+++ ShoppingCart/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart
+{
+    public class ShoppingCart
+    {
+        public int CalculateAmount(List<book> bookCollection)
+        {
+            int amount = 0;
+            return amount;
+        }
+    }
+
+    public class book
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ ShoppingCart/ShoppingCartTests/ShoppingCartTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCart;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ShoppingCart.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void 第一集買了一本_共買一本_價格為100()
+        {
+            // Arrange
+            var target = new ShoppingCart();
+            var books = new List<book>()
+            {
+                new book() { Id = 1,Name="HarryPorter1",Price=100},
+            };
+
+            // Act
+            var actual = target.CalculateAmount(books);
+
+            // Assert
+            var expectedResult = 100;
+            Assert.AreEqual(expectedResult, actual);
+        }
+    }
+}
