579984291b2c34e69e3bb3ecc9f62ac6ead476fb
peiching <leslie7892001@hotmail.com>
2016-01-20 12:38:59 +0800

    加入遺漏檔案

---
+++ ShoppingCart/ShoppingCart/NoDiscount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart
+{
+    public class NoDiscount : IAmount
+    {
+        public int CalculateAmount(IEnumerable<book> diffBook)
+        {
+            return Convert.ToInt32(Math.Ceiling(diffBook.Sum(o => o.Price) * 1.0));
+        }
+    }
+}
